59f926318483164eaa13342b11e52f1d0ce6fb4e
Nriver <junqing.zeng@gmail.com>
2019-02-22 18:25:07 +0800

    update solution 102

--- 102. Binary Tree Level Order Traversal.py
+++ 102. Binary Tree Level Order Traversal.py
@@ -2,12 +2,13 @@
 # @Author: Zengjq
 # @Date:   2019-02-21 11:14:01
 # @Last Modified by:   Zengjq
-# @Last Modified time: 2019-02-22 17:58:31
+# @Last Modified time: 2019-02-22 18:24:51
 
 import collections
 
 
 class Solution:
+
     # 速度 100% 内存 87%
     # 广度优先 不记录层数 并且使用deque
 
@@ -16,13 +17,15 @@ class Solution:
             return []
         res = []
         queue = collections.deque()
-
         queue.append(root)
 
         while queue:
             level_size = len(queue)
             current_level = []
 
+            # 把一整层的node先拿出来再处理
+            # 遍历可能会变化的queue不能用for in
+            # 要用range做下标来遍历
             for x in range(level_size):
                 node = queue.popleft()
                 current_level.append(node.val)
@@ -44,18 +47,36 @@ class Solution:
         queue.append([root, 0])
         while queue:
             node, level = queue.pop(0)
-            if len(res) == level:
-                res.append([node.val, ])
-            elif res[level]:
-                res[level].append(node.val)
-            else:
-                res[level] = [node.val, ]
+
+            if len(res) < level + 1:
+                res.append([])
+
+            res[level].append(node.val)
+
             if node.left:
                 queue.append([node.left, level + 1])
             if node.right:
                 queue.append([node.right, level + 1])
         return res
 
+    # 深度优先 使用递归
+    def levelOrder3(self, root: 'TreeNode') -> 'List[List[int]]':
+        if not root:
+            return []
+        self.res = []
+        self._dfs(root, 0)
+        return self.res
+
+    def _dfs(self, node, level):
+
+        if len(self.res) < level + 1:
+            self.res.append([])
+        self.res[level].append(node.val)
+
+        if node.left:
+            self._dfs(node.left, level + 1)
+        if node.right:
+            self._dfs(node.right, level + 1)
 
 # Tree definition found in here
 # https://leetcode.com/problems/recover-binary-search-tree/discuss/32539/Tree-Deserializer-and-Visualizer-for-Python
@@ -90,6 +111,7 @@ def deserialize(string):
 
 test_cases = (deserialize('[3,9,20,null,null,15,7]'),
               deserialize('[]'),
+              deserialize('[1, null, 2]'),
               )
 
 solution = Solution()
